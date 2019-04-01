a414a9ce60e192970b6930c6eb5604a216aa31f1
jlin03 <jlin03@stuy.edu>
2019-04-01 14:37:46 -0400

    buckets for negative implemented

--- Radix.java
+++ Radix.java
@@ -1,16 +1,14 @@
 import java.util.*;
 public class Radix {
 
-	public static void sort(int[] data) {
-		LinkedList[] freq = new LinkedList[10];
-		LinkedList[] negFreq = new LinkedList[10];
+	public static void radixsort(int[] data) {
+		LinkedList[] freq = new LinkedList[20];
 		LinkedList l =  new LinkedList();
 		for(int i = 0; i < data.length; i++) {
 			l.add(data[i]);
 		}
 		for(int i = 0; i < freq.length; i++) {
 			freq[i] = new LinkedList();
-			negFreq[i] = new LinkedList();
 		}
 		boolean passAgain = true;
 		int step = 1;
@@ -21,50 +19,28 @@ public class Radix {
 				if(current.getVal()/(int)Math.pow(10,step) != 0) {
 					passAgain = true;
 				}
-				int index = (current.getVal()/(int)Math.pow(10,step-1)) % 10;
+				int index = Math.abs((current.getVal()/(int)Math.pow(10,step-1)) % 10);
 				if(current.getVal() < 0) {
-					negFreq[index*-1].add(current.getVal());
+					freq[9-index].add(current.getVal());
 				}
 				else {
 					freq[index].add(current.getVal());
 				}
 				current = current.getNext();
 			}
-			for(int i = -9; i < freq.length-1; i++) {
-				if(i < 0) {
-					if(negFreq[i*-1].getStart() != null && negFreq[i*-1-1].getStart() != null) {
-						negFreq[i*-1-1] = LinkedList.merge(negFreq[i*-1],negFreq[i*-1-1]);
-						negFreq[i*-1] = null;
-					}
-					else if(negFreq[i*-1].getStart() != null && negFreq[i*-1-1].getStart() == null) {
-						negFreq[i*-1-1] = negFreq[i*-1];
-						negFreq[i*-1] = null;
-					}
+			for(int i = 0; i < freq.length-1; i++) {
+				if(freq[i].getStart() != null && freq[i+1].getStart() != null) {
+					freq[i+1] = LinkedList.merge(freq[i],freq[i+1]);
+					freq[i] = null;
 				}
-				if(i == 0 && negFreq[i].getStart() != null) {
-					if(freq[i].getStart() != null) {
-						freq[0] = LinkedList.merge(negFreq[0],freq[0]);
-					}
-					else {
-						freq[0] = negFreq[0];
-					}
-					negFreq[0] = null;
-				}
-				if(i >= 0) {
-					if(freq[i].getStart() != null && freq[i+1].getStart() != null) {
-						freq[i+1] = LinkedList.merge(freq[i],freq[i+1]);
-						freq[i] = null;
-					}
-					else if(freq[i].getStart() != null && freq[i+1].getStart() == null) {
-						freq[i+1] = freq[i];
-						freq[i] = null;
-					}
+				else if(freq[i].getStart() != null && freq[i+1].getStart() == null) {
+					freq[i+1] = freq[i];
+					freq[i] = null;
 				}
 			}
-			l = freq[9];
+			l = freq[19];
 			for(int i = 0; i < freq.length; i++) {
 				freq[i] = new LinkedList();
-				negFreq[i] = new LinkedList();
 			}
 			step++;
 		}
@@ -74,7 +50,7 @@ public class Radix {
 			current = current.getNext();
 		}
 	}
-	
+
 
 
 }
